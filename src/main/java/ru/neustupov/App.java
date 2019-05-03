1c5231301d73eed1a738db0ebb64be9accaa4b98
neustupov <neustupov@yandex.ru>
2019-05-03 13:43:38 +0300

    added servlet for simple get and post methods, added page generator with freemarker, edited main method for run app, added simple html page, added jetty servlet support and freemarker dependency in pom file

--- src/main/java/ru/neustupov/App.java
+++ src/main/java/ru/neustupov/App.java
@@ -1,13 +1,22 @@
 package ru.neustupov;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import ru.neustupov.servlet.AllRequestsServlet;
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+    Server server = new Server(8080);
+    server.setHandler(context);
+
+    server.start();
+    server.join();
+  }
 }
