8b16d82ca437aa262a162dbce796c341da380217
neustupov <neustupov@yandex.ru>
2019-05-04 18:43:47 +0300

    added two other servlets for auth users by login and sessions

---
+++ src/main/java/main/App.java
@@ -0,0 +1,38 @@
+package main;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.SessionServlet;
+import servlets.UsersServlet;
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+
+    AccountService accountService = new AccountService();
+    accountService.addNewUser(new UserProfile("admin"));
+    accountService.addNewUser(new UserProfile("test"));
+
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
+    context.addServlet(new ServletHolder(new SessionServlet(accountService)), "/api/v1/sessions");
+
+    ResourceHandler resourceHandler = new ResourceHandler();
+    resourceHandler.setResourceBase("public_html");
+
+    HandlerList handlerList = new HandlerList();
+    handlerList.setHandlers(new Handler[]{resourceHandler, context});
+
+    Server server = new Server(8080);
+    server.setHandler(handlerList);
+
+    server.start();
+    server.join();
+  }
+}
